db1f191b0dd99d58251273fcf8e9b4dc0a8f6b1f
ProPalestine <muhammadowaispatni31@gmail.com>
2024-06-23 15:43:44 +0500

    Add files via upload
    Created by Muhammad. It's assignment no 2 of Bano Qabil 3.0 class

---
+++ first.py
@@ -0,0 +1,23 @@
+#first assigment
+
+b = 4 
+h = 8
+
+print("area of triangle")
+print(0.5*b*h)
+
+#secound
+
+radius = 8
+pie = 3.14
+
+print("area of circle")
+print(pie*radius*radius)
+
+#third assigment
+s = 6
+
+print("area of square")
+print(s*s)
+                                                                                                                                                                                                                                           
+
